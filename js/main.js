b1f22876d7ba9dead6cb9c096c30c95bad3c6263
LaGoule <damien.beuchat@gmail.com>
2017-03-31 01:03:39 +0200

    Ajout de la victoire
    A chaque fois qu'on recharge la page un des deux joueurs gagne.

--- js/main.js
+++ js/main.js
@@ -51,10 +51,9 @@ var gameState = {
       if(i!=j2Roll.length-1){j2Score+=' - '};
     }
 
-
-
+    //On compte combien chaque joueur à de zero et de un et ont le met dans jxTotal
+    //J1
     var j1True = j1False = 0;
-
     for (var i=0;i<j1Roll.length;i++) {
         if (j1Roll[i] === 1) {
             j1True++;
@@ -62,25 +61,58 @@ var gameState = {
           j1False++;
         }
     };
-    var j1Total = '';
+    var j1Total = 0;
     if(j1.role){
       j1Total = j1True;
+      var j1TotalT = j1Total+ ' ATT';
     }else{
       j1Total = j1False;
+      var j1TotalT = j1Total+ ' DEF';
     };
 
-    console.log(j1True, j1False);
+    //J2
+    var j2True = j2False = 0;
+    for (var i=0;i<j2Roll.length;i++) {
+        if (j2Roll[i] === 1) {
+            j2True++;
+        }else{
+          j2False++;
+        }
+    };
+    var j2Total = 0;
+    if(j2.role){
+      j2Total = j2True;
+      var j2TotalT = j2Total+ ' ATT';
+    }else{
+      j2Total = j2False;
+      var j2TotalT = j2Total+ ' DEF';
+    };
 
+    //Resultat du conflit (On compare les deux totaux)
+    var winTX=0;
+    var winT='WIN!';
+
+    if(j1Total>j2Total){
+      winTX=180;
+    }else if(j1Total==j2Total){
+      winTX=250;
+      winT='DRAW--!'
+    }else{
+      winTX=320;
+    }
 
     //Partie dessin
+    var stAtt = { font: "65px Arial", fill: "#ff0022", align: "center" };
+    var stDef = { font: "65px Arial", fill: "#00ff66", align: "center" };
+    var stWin = { font: "30px Arial", fill: "#ffff66", align: "center" };
     //Lancer du J1
-    var style = { font: "65px Arial", fill: "#ff0022", align: "center" };
-    var t = game.add.text(100, 100, j1Score, style);
-    var t2 = game.add.text(370, 100, '= '+j1Total, style);
+    var t = game.add.text(100, 100, j1Score, stAtt);
+    var t2 = game.add.text(370, 100, '= '+j1TotalT, stAtt);
     //Lancer du J2
-    var style = { font: "65px Arial", fill: "#00ff66", align: "center" };
-    var t = game.add.text(100, 360, j2Score, style);
-
+    var t = game.add.text(100, 360, j2Score, stDef);
+    var t2 = game.add.text(370, 360, '= '+j2TotalT, stDef);
+    //Vainquer
+    var tWin = game.add.text(100, winTX, winT, stWin);
 
   },
 
