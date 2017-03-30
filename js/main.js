9fcadb85fa1e0c38dda3c42fb0baa1cc3c9922c1
LaGoule <damien.beuchat@gmail.com>
2017-03-30 19:25:57 +0200

    Revision gameState
    Revision des états et du squelette de mon projet phaser

--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
+//On créer l'objet qui contiendra tout le jeu
+var game = new Phaser.Game(960, 540, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
+//On créer l'objet qui contiendra les états du jeu
+var gameState = {
+  //On charge tout les assets
+  preload: function() {
+    this.load.image('tux','res/tux.png');
+  },
 
-function preload() {
-}
+  //On lance create une fois que les assets sont tous chargés (une fois)
+  create: function() {
+    this.add.sprite(0,0,'tux');
+  },
 
-function create() {
-}
+  //Update se lance toute les frames après l'événement create
+  update: function() {
+  }
+};
 
-function update() {
-}
-
-function createPlayer(x,y){
-}
-
-function playerUpdate(){
-}
-
-function createPlatform(){
-}
+//On charge l'état dans le jeu
+game.state.add('GameState',gameState);
+//On lance le jeu
+game.state.start('GameState');
