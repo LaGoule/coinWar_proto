6b26e2a1bfde5429131d9a8a4ffdfba5c68e0c37
LaGoule <damien.beuchat@gmail.com>
2017-03-31 17:38:40 +0200

    bug bizarre

---
+++ js/gametitle.js
@@ -0,0 +1,16 @@
+var gametitle = function(game){}
+
+gametitle.prototype = {
+  create: function(){
+    //On charge un fond au bol
+    var bg = this.game.add.sprite(0,0,'bg');
+		var gameTitle = this.game.add.sprite(100,160,"gametitle");
+		gameTitle.anchor.setTo(0.5,0.5);
+		var playButton = this.game.add.button(100,320,"play",this.playTheGame,this);
+		playButton.anchor.setTo(0.5,0.5);
+	},
+	playTheGame: function(){
+		this.game.state.start("Match");
+	}
+}
+console.log('3. GameTitle executed.');
